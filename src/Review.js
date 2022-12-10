54420a9d0ef7318f02256d097643a891fbffbd20
hwiiy <87405971+hwiiy@users.noreply.github.com>
2022-12-10 23:27:59 +0900

    8.Create

--- src/Review.js
+++ src/Review.js
@@ -26,8 +26,6 @@ function Content(props) {
       <article>
         <h2>{props.title}</h2>
 
-        <img src={props.img} style={{ width: "200px", height: "150px" }}></img>
-
         <div>{props.body}</div>
       </article>
     </div>
@@ -58,54 +56,82 @@ function Links(props) {
 
   return <ul className={classes.content}>{li}</ul>;
 }
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
 
-function Review() {
+function App() {
   const [mode, setMode] = useState(null);
   const [id, setId] = useState(null);
   let content = null;
 
-  const foods = [
+  const [nextId, setNextId] = useState(4);
+  const [foods, setFoods] = useState([
     {
       id: 1,
       title: "kimbab",
       body: "i love kimbab ...",
-      img: "https://www.seriouseats.com/thmb/WtcBGMWbdUcAIVe8zg4VCm2aqc4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2020__01__20200122-gimbap-vicky-wasik-24-f5ed1075f35846a29e0812ee053a1bf8.jpg",
     },
     {
       id: 2,
       title: "noodle",
       body: "i dont like noodle ...",
-      img: "https://www.simplyrecipes.com/thmb/rqYG_EvkZ0lNR0WRrXMBw9r5ZVk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Korean-Spicy-Cold-Noodles-LEAD-17-39a55b09d58745b68811a2fddf304250.jpg",
     },
     {
       id: 3,
       title: "chicken",
       body: "i love chicken...",
-      img: "https://www.seriouseats.com/thmb/IZEAHrIKyaHMFIWFl5ss7VpvgN4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/20210714-potato-starch-fried-chicken-vicky-wasik-seriouseats-20-17e193a6bf274bba9091810a0b18ef89.jpg",
     },
-  ];
+  ]);
 
   if (mode === "menu") {
-    content = (
-      <Content
-        title="hello"
-        body=" this is menu!!"
-        img="https://www.shutterstock.com/image-vector/fast-food-restaurant-menu-sandwiches-600w-586392848.jpg"
-      />
-    );
+    content = <Content title="hello" body=" this is menu!!" />;
   } else if (mode === "link") {
     let title,
-      body,
-      img = null;
+      body = null;
     for (let i = 0; i < foods.length; i++) {
       if (foods[i].id === id) {
         title = foods[i].title;
         body = foods[i].body;
-        img = foods[i].img;
       }
     }
-
-    content = <Content title={title} body={body} img={img} />;
+    content = <Content title={title} body={body} />;
+  } else if (mode === "CREATE") {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          const newTopic = { id: nextId, title: _title, body: _body };
+          const newTopics = [...foods];
+          newTopics.push(newTopic);
+          setFoods(newTopics);
+          setMode("READ");
+          setId(nextId);
+          setNextId(nextId + 1);
+        }}
+      ></Create>
+    );
   }
 
   return (
@@ -123,8 +149,17 @@ function Review() {
         }}
       ></Links>
       {content}
+      <a
+        href="/create"
+        onClick={(event) => {
+          event.preventDefault();
+          setMode("CREATE");
+        }}
+      >
+        Create
+      </a>
     </div>
   );
 }
 
-export default Review;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Review from "./Review";
+import App from "./Review";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Review></Review>
+    <App></App>
   </React.StrictMode>
 );
