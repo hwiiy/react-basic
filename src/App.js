cf0049c0f08fef1a5091eb0e81d90892e631f8ee
hwiiy <87405971+hwiiy@users.noreply.github.com>
2022-12-10 14:08:48 +0900

    state 까지 복습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function Header(props) {
+  return (
+    <div>
+      <h1>
+        <a
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+          href="#"
+        >
+          WEB!!
+        </a>
+      </h1>
+    </div>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.content}
+    </article>
+  );
+}
+
+function Nav(props) {
+  const lis = [];
+
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(event.target.id);
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+  }
+  return (
+    <div>
+      <ol>{lis}</ol>
+    </div>
+  );
+}
 
 function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "javascript", body: "javascript is ..." },
+  ];
+
+  const [mode, setMode] = useState("null");
+
+  let content = null;
+  if (mode === "web") {
+    content = <Article title="hello" content="web!"></Article>;
+  } else if (mode === "read") {
+    content = <Article title="hello" content="read!"></Article>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header onChangeMode={() => setMode("web")}></Header>
+      <Nav topics={topics} onChangeMode={() => setMode("read")}></Nav>
+      {content}
     </div>
   );
 }
